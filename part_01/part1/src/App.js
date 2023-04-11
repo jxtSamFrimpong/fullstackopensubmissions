43932903542bc7ea81cac76e6be9eec52a57124f
jenkins <sfrimpong112@gmail.com>
2023-04-11 10:47:41 +0000

    conditional rendering

--- part_01/part1/src/App.js
+++ part_01/part1/src/App.js
@@ -1,14 +1,81 @@
 import { useState } from 'react'
 
-const App = () => {
-    const [counter, setNumber] = useState(0)
+const History = ({allClicks, total}) => {
+    if (allClicks.length === 0){
+        return (
+            <div>The application is used by pressing the buttons</div>
+            )
+    }
 
-    setTimeout(
-        () => setNumber(Math.floor(Math.random() * 10)),
-        1000
-    )
     return (
-        <div>{counter}</div>
+    <div>
+        <p>button press History: {allClicks.join(' ')}</p>
+        <p>The button has been pressed a total of {total}</p>
+    </div>
+    )
+}
+
+const App = () => {
+    // const [left, setLeft] = useState(0)
+    // const [right, setRight] = useState(0)
+    const [ clicks, setClicks ] = useState({
+        left: 0,
+        right: 0
+    })
+    const [allClicks, setAll] = useState([])
+    const [total, setTotal] = useState(0)
+
+    const handleLeftClick = () => {
+
+        setAll([...allClicks, 'L'])
+
+        console.log('left before ', clicks.left)
+
+        const leftClicks = clicks.left + 1
+
+        setClicks({
+            ...clicks,
+            left: leftClicks
+        })
+
+        console.log('left after ', leftClicks)
+
+        setTotal(leftClicks + clicks.right)
+    }
+
+    const handleRightClick = () => {
+        setAll([...allClicks, 'R'])
+
+        console.log('right before ', clicks.right)
+
+        const rightClicks = clicks.right + 1
+
+
+        setClicks({
+            ...clicks,
+            right: rightClicks
+        })
+
+        console.log('right after ', rightClicks)
+
+        setTotal(clicks.left + rightClicks)
+    }
+
+    return (
+        <div>
+        <div>
+            {clicks.left}
+            <button onClick={() => handleLeftClick()}>
+                left
+            </button>
+            <button onClick={() => handleRightClick()}>
+                right
+            </button>
+            {clicks.right}
+        </div>
+        <br/>
+            <History allClicks={allClicks} total={total} />
+        </div>
     )
 }
 
