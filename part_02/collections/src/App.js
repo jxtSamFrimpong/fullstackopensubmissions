01f03c42f1e35d1de5cae37349c91776878e38b3
jxtsamfrimpong <hub4realtil@gmail.com>
2023-04-12 16:22:08 +0000

    controlled component

--- part_02/collections/src/App.js
+++ part_02/collections/src/App.js
@@ -1,7 +1,34 @@
+import { useState } from 'react'
+
 import Note from "./components/Note";
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
   console.log('notes inside app before returning to index for rendering', notes);
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('form submit button clicked ', event.target)
+    console.log('newnote to submit', newNote)
+    console.log('all notes before submit', notes)
+
+    let noteToSet = {
+      id: [...notes].pop().id + 1,
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    setNotes(notes.concat(noteToSet))
+    setNewNote('')
+
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
 
   return (
     <div>
@@ -12,6 +39,13 @@ const App = ({ notes }) => {
         <li>{notes[2].content}</li> */}
         {notes.map((note) => <Note key={note.id} content={note.content} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
