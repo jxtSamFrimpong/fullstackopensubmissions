a166bba5fd2ceafb5fb4d4526fed4927f25267ed
jxtsamfrimpong <hub4realtil@gmail.com>
2023-05-21 10:52:43 +0000

    unit testing

---
+++ part_03/hello_backend/utils/for_testing.js
@@ -0,0 +1,22 @@
+/* eslint-disable indent */
+const reverse = (string) => {
+    return string
+        .split('')
+        .reverse()
+        .join('')
+}
+
+const average = (array) => {
+    const reducer = (sum, item) => {
+        return sum + item
+    }
+
+    return array.length === 0
+        ? 0
+        : array.reduce(reducer, 0) / array.length
+}
+
+module.exports = {
+    reverse,
+    average,
+}
